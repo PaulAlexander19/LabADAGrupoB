a449aaf0dacf99ff5ca49eb4ff97d6dce7a49852
Paul Alexander <pluquec@unsa.edu.pe>
2021-10-25 01:51:45 -0500

    Implementado en python

--- Clases/Lab04/HallarComplejidadTwoSum.py
+++ Clases/Lab04/HallarComplejidadTwoSum.py
@@ -1,15 +1,24 @@
 ## Hallar la complejidad del algoritmod
 
 ## Algoritmo origianal escrito en javascript
-function twoSum(array){
-    for (let i = 0; i < array.length; i++) {
-        for (let j = 0; j < array.length; j++) {
-            if (i !== j && array[i] + array[j] === 10) {
-                return true;
-            }
-        }
-    }
+# function twoSum(array){
+#     for (let i = 0; i < array.length; i++) {
+#         for (let j = 0; j < array.length; j++) {
+#             if (i !== j && array[i] + array[j] === 10) {
+#                 return true;
+#             }
+#         }
+#     }
 
-    return false;
-}
+#     return false;
+# }
 
+
+## Algoritmo en python
+
+def twoSum(array):
+    for i in range(len(array)):
+        for j in range(len(array)):
+            if i != j and array[i] + array[j] == 10:
+                return True
+    return False
