c1d4eb0b81bfb61762211a205b142f67eca719ab
Paul Alexander <pluquec@unsa.edu.pe>
2021-10-25 00:45:48 -0500

    Implementación y prueba del algoritmo en python

--- Clases/Lab04/HallarComplejidad.py
+++ Clases/Lab04/HallarComplejidad.py
@@ -1,16 +1,30 @@
 ## Hallar la complejidad del codigo escrito en google
 
 ## Código en GO
+# def every_other(array):
+#     array.each_with_index do |element, index|
+#         if index.even?
+#             arrat.each do |other_number|
+#                 puts number + other_number
+#             end
+#         end
+#     end
+# end
+
+## Implementación en Python
+
 def every_other(array):
-    array.each_with_index do |element, index|
-        if index.even?
-            arrat.each do |other_number|
-                puts number + other_number
-            end
-        end
-    end
-end
+    for index in range(0, len(array), 2):
+        for other_number in array:
+            print(array[index] + other_number)
+
+        print("----\n")
 
 
+## Prueba del algoritmo 
+
+arr = [1,2,3]
+
+every_other(arr)
 
 
