aef80350b468e40d1c49831801fe8ba440352df4
Paul Alexander <pluquec@unsa.edu.pe>
2021-10-25 00:42:27 -0500

    Código copiado en GO

---
+++ Clases/Lab04/HallarComplejidad.py
@@ -0,0 +1,16 @@
+## Hallar la complejidad del codigo escrito en google
+
+## Código en GO
+def every_other(array):
+    array.each_with_index do |element, index|
+        if index.even?
+            arrat.each do |other_number|
+                puts number + other_number
+            end
+        end
+    end
+end
+
+
+
+
