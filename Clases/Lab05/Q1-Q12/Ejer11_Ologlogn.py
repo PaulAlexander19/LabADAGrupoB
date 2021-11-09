624c10ba16345f5e2f35d7ec43b491a0378aacc8
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-09 12:13:14 -0500

    Ejercicio 11, 12 separados en archivos diferentes

---
+++ Clases/Lab05/Q1-Q12/Ejer11_Ologlogn.py
@@ -0,0 +1,40 @@
+## Calcular la complejidad del código de la función
+## Autor: Luque Ccosi, Paul Alexander
+
+def ejercicio11(n):
+    p = 0
+    i = 1
+    while(i < n):
+        p = p + 1
+        i = i * 2
+
+    j = 1
+    while(j < p):
+        j = j * 2
+
+    return p
+
+print(ejercicio11(10))
+
+    # Análisis personal
+        # La complejidad es log(log(n)) es un ejecto cadena, el primer buble ya lo resolvimos
+        # y sale log(p) en el segundo bucle es la misma idea log(p), seria log(log(p))
+
+    
+    ## Demostración en clase:
+        # // A11:
+        # p = 0
+        # // (2)
+        # for (i = 1; i < n; i = i * 2) {
+        #     p++;                    // p = log(n)
+        # }
+
+        # // (1)
+        # for (j = 1; j < p; j = j * 2) {
+        #     statement;              // log(p)
+        # }
+
+        # // So, we have log(p) and p = log(n)
+        # // replacing log log(n)
+        # // O(log log(n))
+
---
+++ Clases/Lab05/Q1-Q12/Ejer12_Onlogn.py
@@ -0,0 +1,27 @@
+## Calcular la complejidad del código de la función
+## Autor: Luque Ccosi, Paul Alexander
+
+def ejercicio12(n):
+    i = 0
+    while(i < n):
+        j = 1
+        while(j < n):
+            j = j * 2
+        i = i + 1
+
+    return i
+
+print(ejercicio12(10))
+
+    # Análisis personal
+        # La complejidad es O(n*log(n)) por que el ciclo interno es O(log(n)) y el ciclo externo es O(n)
+        # La complejidad es nlog(n) entonces O(nlog(n))
+
+    
+    ## Demostración en clase:
+        # // (1) This repeats n times
+        # // (2) and this inner loop n times * log(n)
+        # // (3) and this statement n times * log(n)
+        # // Adding them together n + 2 n * log(n)
+        # // O(n log(n))
+
