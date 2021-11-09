54323988cab8e0c7e823c321f8d5ac2884d7405a
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-09 11:52:50 -0500

    Organizando archivos, y ejercicio1 de complejidad

---
+++ Clases/Lab05/Q1-Q12/Ejer1_on.py
@@ -0,0 +1,23 @@
+## Calcular la complejidad del código de la función
+## Autor: Luque Ccosi, Paul Alexander
+
+def ejercicio1 (n):
+    # Q1: What is the time complexity of
+    i = 0
+    while (i < n):
+        i = i + 1
+
+    return i
+
+print(ejercicio1(10))
+
+    # Análisis personal
+        # La complejidad temporal de este algoritmo es O(n) 
+        # porque el bucle se ejecuta n veces.
+
+    ## Demostración en clase:
+        # A1: O(n)
+        # for (i = 0; i < n; i++) {   // n + 1
+        #     statement;               // n
+        # }
+
