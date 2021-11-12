17de56d0b62d08c4d1a77410020c398c576a1ec4
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-12 12:06:11 -0500

    Implementado el pintado del cuadrado

--- Clases/Lab06/DFS/PintarCuadro_Dfs_Recursive.py
+++ Clases/Lab06/DFS/PintarCuadro_Dfs_Recursive.py
@@ -2,33 +2,6 @@
 ## Autor: Luque Ccosi Paul Alexander
 
 
-## Para que funcione correcamente nos apoyaremos de una clase adicional
-
-class Node():
-    def __init__(self, valueInitial):
-        self.valueInitial = valueInitial
-        self.visited = False
-        self.neighbors = []
-    
-    def addNeighbor(self, node):
-        self.neighbors.append(node)
-    
-    
-    def getNeighbors(self):
-        return self.neighbors
-    
-    def getValue(self):
-        return self.valueInitial
-    
-    def setVisited(self):
-        self.visited = True
-    
-    def isVisited(self):
-        return self.visited
-    
-    def __str__(self):
-        return str(self.valueInitial)
-    
     
 
 ## algoritmo como tal
@@ -40,11 +13,11 @@ def dfs_recursive(matrix, startX, startY, simbVisited, simbPared):
     dy = [0,1,0,-1]
     
    
-    for i in range(4):
+    for i in range(4):      ## buscamos en sus 4 adyecentes
         nx = dx[i] + startX
         ny = dy[i] + startY
         
-        if(matrix[nx][ny] != simbVisited and matrix[nx][ny] != simbPared):
+        if(matrix[nx][ny] != simbVisited and matrix[nx][ny] != simbPared):  ## Verificamos si ya esta visitado o si es parte de la pared
             dfs_recursive(matrix, nx, ny, simbVisited, simbPared)
         
     return matrix
@@ -53,13 +26,22 @@ def dfs_recursive(matrix, startX, startY, simbVisited, simbPared):
 ## Generate matrix 5 of 5 with 
 matri = [
     ["#","#","#","#","#","#"],
-    ["#"," "," "," "," ","#"],
-    ["#"," "," "," "," ","#"],
-    ["#"," "," "," "," ","#"],
-    ["#"," "," "," ","#"," "],
-    ["#","#","#","#"," "," "],
+    ["#","-","-","-","-","#"],
+    ["#","-","-","-","-","#"],
+    ["#","-","-","-","-","#"],
+    ["#","-","-","-","#","-"],
+    ["#","#","#","#","-","-"],
 ]
 
 
 
-print(dfs_recursive(matri, 2,2,"o", "#"))
+# Metodo para imprimir la matriz
+def printMatrix(matrix):
+    for i in matrix:
+        for j in i:
+            print(" " , j , end=" ")
+        print()
+        
+        
+printMatrix(matri)      ## antes
+printMatrix(dfs_recursive(matri, 5,5,"o", "#"))  ## despues
