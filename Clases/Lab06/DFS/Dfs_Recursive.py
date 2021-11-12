f03b4f21137efe614fe8d0ffa0e6e17d5ca63311
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-12 10:18:12 -0500

    DFS_recursivo

--- Clases/Lab06/DFS/Dfs_Recursive.py
+++ Clases/Lab06/DFS/Dfs_Recursive.py
@@ -31,26 +31,38 @@ class Node():
     
     
 
-def dfs_recursive(graph, start):
-    start.visited = True
+## algoritmo como tal
+def dfs_recursive(start):
+    start.setVisited()
     print("marcando nodo" + str(start))
     for n in start.getNeighbors():
-        if not n.visited:
-            dfs_recursive(graph, n)
+        if not n.isVisited():
+            dfs_recursive(n)
 
+
+## Casos de prueba
 
 node1 = Node(1)
 node2 = Node(2)
 node3 = Node(3)
 node4 = Node(4)
 node5 = Node(5)
+node6 = Node(6)
+node7 = Node(7)
 
 ## generate a graph with nodes and edges
-
-
 node1.addNeighbor(node2)
-node2.addNeighbor(node5)
+node1.addNeighbor(node3)
+node1.addNeighbor(node4)
 node2.addNeighbor(node4)
-node2.addNeighbor(node3)
+node2.addNeighbor(node5)
+node3.addNeighbor(node6)
+node3.addNeighbor(node7)
 
-dfs_recursive(node1, node1)
+## Arbol 
+              # 1
+        #2              #5
+    #4      5       #6    #7
+    
+print("Esperado: node1, node2, node3, node4, node5, node6, node7")   
+dfs_recursive(node1)
