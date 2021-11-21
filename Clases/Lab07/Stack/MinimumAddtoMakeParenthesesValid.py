1d0d09e3cb118bdd2c069a5b13e439714ad95b6e
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-20 23:59:18 -0500

    Primera prueba para el validado de los parentesis

---
+++ Clases/Lab07/Stack/MinimumAddtoMakeParenthesesValid.py
@@ -0,0 +1,36 @@
+## Minimum Add to Make Parentheses Valid
+
+## Encontrar la minima cantidad de parentesis para que sea valida la entrada de parentesis
+
+## Aqui podemos agregar mas elementos
+first = ["("]
+finish = [")"]
+
+def parenthesesValid(str):
+    s = [] ## stack
+    for i in str:
+        print(i)
+        if(i in first):
+            s.append(i)
+        elif(i in finish):
+            if(len(s) == 0 or s[-1] in finish):
+                s.append(i)
+            elif(s[-1] in first):
+                s.pop()
+    return s
+        
+        
+
+## Datos de Prueba
+res = parenthesesValid("())")
+print(res)
+print(len(res))
+
+## Datos de Prueba
+res = parenthesesValid("(()))(")
+print(res)
+print(len(res))
+
+res = parenthesesValid("()))((")
+print(res)
+print(len(res))
