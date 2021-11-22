df6d281484b92cd50f302bfbfb718b0ee7ba229b
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-22 00:03:37 -0500

    Implementado el codigo para InterviewWait

---
+++ Clases/Lab07/Deque/InterviewWait.py
@@ -0,0 +1,24 @@
+## InterviewWait
+
+## Luque Ccosi Paul Alexander
+
+def InterviewWait(listaWait):
+    resul = 0
+    
+    while True:
+        ## Verificamos si ya termianmos
+        if(listaWait[0] < 0):
+            return resul
+        elif(listaWait[-1] < 0):
+            return resul
+        ## Ingresamos el minimo valor
+        else:
+            if(listaWait[0] < listaWait[-1]):                
+                resul += listaWait.pop(0)
+            else:
+                resul += listaWait.pop(-1)
+    return resul
+
+## Datos de prueba
+print("lista: " + [4,-1,5,2,3,1,0].__str__())
+print(InterviewWait([4,-1,5,2,3]))
