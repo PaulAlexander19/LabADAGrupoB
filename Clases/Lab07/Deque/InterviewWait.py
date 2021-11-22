dc5fc52e68af6e585f7ff6a7ec6be304dbf8ad7e
Paul Alexander <pluquec@unsa.edu.pe>
2021-11-22 00:03:43 -0500

    Agregando comentarios

--- Clases/Lab07/Deque/InterviewWait.py
+++ Clases/Lab07/Deque/InterviewWait.py
@@ -1,24 +1,26 @@
-## InterviewWait
+# InterviewWait
 
-## Luque Ccosi Paul Alexander
+# Verificar el tiempo enque nos tocara esperar para la entrevista, nosotros somos el valor -1, siempre elige al menos valor de los extremos
+# Luque Ccosi Paul Alexander
 
 def InterviewWait(listaWait):
     resul = 0
-    
+
     while True:
-        ## Verificamos si ya termianmos
+        # Verificamos si ya termianmos
         if(listaWait[0] < 0):
             return resul
         elif(listaWait[-1] < 0):
             return resul
-        ## Ingresamos el minimo valor
+        # Ingresamos el minimo valor
         else:
-            if(listaWait[0] < listaWait[-1]):                
+            if(listaWait[0] < listaWait[-1]):
                 resul += listaWait.pop(0)
             else:
                 resul += listaWait.pop(-1)
     return resul
 
-## Datos de prueba
-print("lista: " + [4,-1,5,2,3,1,0].__str__())
-print(InterviewWait([4,-1,5,2,3]))
+
+# Datos de prueba
+print("lista: " + [4, -1, 5, 2, 3, 1, 0].__str__())
+print(InterviewWait([4, -1, 5, 2, 3]))
