e9421c170f67a5d98f19a13e6211ce0af025d91e
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-04 17:53:42 -0500

    agegando comentarios en el ejercicio 5

--- Clases/Lab08/5_MaximalSquare/MaximalSquare.java
+++ Clases/Lab08/5_MaximalSquare/MaximalSquare.java
@@ -3,6 +3,7 @@ public class MaximalSquare {
 
     public static void main(String[] args) {
 
+        // Caso de prueba
         char[][] matrix = { { '1', '0', '1', '0', '0' },
                 { '1', '0', '1', '1', '1' },
                 { '1', '1', '1', '1', '1' },
@@ -24,10 +25,12 @@ public class MaximalSquare {
 
         int cols = matrix[0].length;
 
+        // Estructura de apoyo
         int[][] board = new int[rows + 1][cols + 1];
 
         int biggest = 0;
 
+        // Iteramos los posiciones del rectangulo
         for (int i = 1; i < rows + 1; i++) {
 
             for (int j = 1; j < cols + 1; j++) {
