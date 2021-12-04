44987a8271635ec8375aff2530c9b3ece181a49c
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-03 22:13:19 -0500

    Funciona pero no es eficiente

--- Clases/Lab08/BookShop.java
+++ Clases/Lab08/BookShop.java
@@ -6,24 +6,26 @@ public class BookShop {
     public static void main(String[] args) {
         // Entrada de datos
         Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
-        int n = 3;
+        int n = sc.nextInt();
+        // int n = 3;
 
-        // int maxPrice = sc.nextInt();
-        int maxPrice = 3;
+        int maxPrice = sc.nextInt();
+        // int maxPrice = 3;
         
-        // int[] price = new int[n];
-        // for (int i = 0; i < n; i++) {
-            //     price[i] = sc.nextInt();
-            // }
-        int[] price = {0,1,2,3};
+        int[] price = new int[n+1];
+        price[0] = 0;
+        for (int i = 1; i <= n; i++) {
+                price[i] = sc.nextInt();
+            }
+        // int[] price = {0,1,2,3};
             
-        // int[] pages = new int[n];
-        // for (int i = 0; i < n; i++) {
-            //     pages[i] = sc.nextInt();
-            // }
+        int[] pages = new int[n+1];
+        pages[0] = 0;
+        for (int i = 1; i <= n; i++) {
+                pages[i] = sc.nextInt();
+            }
             
-        int[] pages = {0,1,4,6};
+        // int[] pages = {0,1,4,6};
 
         // System.out.println(n);
 
