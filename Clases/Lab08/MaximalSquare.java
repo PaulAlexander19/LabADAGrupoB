6ab648c53ff4e8373806e2e2f8b60466cacf8890
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-04 16:38:58 -0500

    Implementando El ejercicio 5

---
+++ Clases/Lab08/MaximalSquare.java
@@ -0,0 +1,59 @@
+
+public class MaximalSquare {
+
+    public static void main(String[] args) {
+
+        // Metodo main del programa
+
+        // Recibimos la matriz con los caracteres 0's y 1's
+
+        char[][] matrix = { { '1', '0', '1', '0', '0' },
+                { '1', '0', '1', '1', '1' },
+                { '1', '1', '1', '1', '1' },
+                { '1', '0', '0', '1', '0' } };
+
+        System.out.println(maximalSquare(matrix));
+
+    }
+
+    public static int maximalSquare(char[][] matrix) {
+
+        int rows = matrix.length;
+
+        if (rows == 0) {
+
+            return 0;
+
+        }
+
+        int cols = matrix[0].length;
+
+        int[][] board = new int[rows + 1][cols + 1];
+
+        int biggest = 0;
+
+        for (int i = 1; i < rows + 1; i++) {
+
+            for (int j = 1; j < cols + 1; j++) {
+
+                if (matrix[i - 1][j - 1] == '1') {
+
+                    board[i][j] = 1 + Math.min(board[i - 1][j], Math.min(board[i][j - 1], board[i - 1][j - 1]));
+
+                    if (biggest < board[i][j]) {
+
+                        biggest = board[i][j];
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        return biggest * biggest;
+
+    }
+
+}
