4fe154500772b4fa66be7eead06a02beb0c24aa4
Paul Alexander <pluquec@unsa.edu.pe>
2021-12-11 22:36:18 -0500

    COrrigiendo la entrada en el ejercicio3, BankQueue

--- Clases/Lab09/BankQueue/BankQueue.py
+++ Clases/Lab09/BankQueue/BankQueue.py
@@ -1,14 +1,7 @@
-# def sorter(elem, max):
-#     a = max -  elem[1]
-#     # b = elem[1]
-#     return elem[0], max
+## TieRopes
 
-
-# data = [[1000, 1], [2000, 2],[2000, 0], [500, 2], [1200, 0],[500,0]]
-
-# max = 1000
-# data = sorted(data, key=lambda item :(item[0], max - item[1]), reverse=True)
-# print(data)
+## @Author: Paul Luque
+## LABS: Lab09
 
 
 import sys
@@ -17,22 +10,21 @@ import sys
 def main():
     
     ## Recibimos los datos
-    flag = True
     dataClientes = []
-    for line in sys.stdin:  # Por cada linea enviada al input iteraremos
-        if flag:
-            n, t = map(int, line.split())
-            tem = []
-            flag = False
-        else:
-            dinero, tiempo = map(int, line.split())
-            tem.append(dinero)
-            tem.append(tiempo)
-            dataClientes.append(tem)
-            tem = []
+    def get_ints(): return map(int, sys.stdin.readline().strip().split())
+    n, t = get_ints()
+    n = int(n)
+    t = int(t)
+    tem = []
+    for line in range(n):  # Por cada linea enviada al input iteraremos
+        dinero, tiempo = get_ints()
+        tem.append(dinero)
+        tem.append(tiempo)
+        dataClientes.append(tem)
+        tem = []
     ## Ordenamos los datos
     dataClientes = sorted(dataClientes, key=lambda item :(item[0], t - item[1]), reverse=True)
-    print(dataClientes)
+    # print(dataClientes)
     print(solution(dataClientes, n, t))
 
 def sorter(elem, max):
@@ -67,5 +59,5 @@ def solution(dataCliente, numberPeople, totalTime):
             
     return totalCash
     
-
-main()
+if __name__ == "__main__":
+    main()
